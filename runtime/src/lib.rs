6f1d6de86ca0f0d0b3e2b8293d3380de90ad3067
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2021-03-16 07:15:53 +0530

    Removed polkapool

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -284,17 +284,6 @@ impl assets::Config for Runtime{
 
 }
 
-parameter_types! {
-	pub const TradingPathLimit: usize = 4;
-}
-
-impl polkapool::Config for Runtime{
-	type Event = Event;
-	type Balance = Balance;
-	type TradingPathLimit = TradingPathLimit;
-
-}
-
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -313,7 +302,6 @@ construct_runtime!(
 		Assets: assets::{Module, Call, Config<T>, Storage, Event<T>},
 		Vesting: pallet_vesting::{Module, Call, Config<T>, Storage, Event<T>},
 		Engine: orderbook_engine::{Module, Call, Storage, Event<T>},
-		Polkapool: polkapool::{Module, Call, Storage, Event<T>},
 	}
 );
 
